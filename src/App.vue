3e4a9ab309944fc8833b11a1728fbcdcbb22e4b8
Luigi Pinna» <luigipinna3@gmail.com>
2022-10-21 23:52:24 +0200

    Vuetify develop test first v

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app-bar
+      absolute
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://images.unsplash.com/photo-1594986138018-cb0b6c2609d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.3), rgba(25,32,72,.3)"
+      ></v-img>
+    </template>
+
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-app-bar-title>Title</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+
+    <template v-slot:extension>
+      <v-tabs align-with-title>
+        <v-tab>Tab 1</v-tab>
+        <v-tab>Tab 2</v-tab>
+        <v-tab>Tab 3</v-tab>
+      </v-tabs>
+    </template>
+
+
+
+  </v-app-bar>
+  <v-container>
+    <v-carousel> </v-carousel>
+  </v-container>
+
+</template>
+
+
+<script>
+import carousel from "@/components/carousel";
+
+export default {
+  components: {carousel}
+
+  //
+}
+</script>
